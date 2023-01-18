261838c0446970708a1f18da219ef0dbc86a98fa
gadShrim <erez116@gmail.com>
2023-01-18 12:32:17 +0200

    task, user and team in database

--- Server/ServerToClient.py
+++ Server/ServerToClient.py
@@ -1,36 +1,181 @@
 import pymongo
-import ServerToDataBase
+from ServerToDataBase import *
+import datetime
+from flask import Flask, request, jsonify
+import json
+import re
+from validate_email import validate_email
+import requests
+
+
+# import MySQLdb
+
+
+# ------------------------ data for user ------------------------
+
+def create_new_user(userdata):
+    res = check_data(userdata)
+    if type(res) == bool:
+        set_user_to_server(userdata)
+        return "New user created successfully"
+    else:
+        return res
+
+
+def check_email(email):
+    # regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    # if (re.fullmatch(regex, email)):
+    #     return True
+    # else:
+    #     return False
+    email_address = email
+    response = requests.get(
+        "https://isitarealemail.com/api/email/validate",
+        params={'email': email_address})
+    status = response.json()['status']
+    if status == "valid":
+        return True
+    return False
+
+
+def check_data(data):
+    send = "Please change the rep, it is incorrect"
+    if not check_user_name(data["username"]):
+        return send.replace('rep', 'username')
+    if not data['name'].isalpha():
+        return send.replace('rep', 'name')
+    elif not data['lastname'].isalpha():
+        return send.replace('rep', 'lastname')
+    elif not check_email(data['useremail']):
+        return send.replace('rep', 'useremail')
+    elif not data['age'].isnumeric():
+        return send.replace('rep', 'age')
+    elif not len(data['userpassword']) > 6:
+        return send.replace('rep', 'userpassword')
+    elif not data['usercity'].isalpha():
+        return send.replace('rep', 'usercity')
+    else:
+        return True
+
+
+def get_user(username):
+    print(get_user_from_server(username))
 
 
 def check_user_name(username):
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["fproject"]
-    mycol = mydb["user"]
-    for x in mycol.find({}, {"_id": 0, "username": 1}):
-        if x.get("username") == username:
-            return False
-    return True
+    response = username_availble(username)
+    return response
 
 
-# test: if username is available
-# x= check_user_name("gadshu")
-# print(x)
-
-def create_new_user(userdata):
-    if check_user_name(userdata["username"]):
-        ServerToDataBase.set_user_to_server(userdata)
+def change_detail(username, field, new_detail):
+    if field == "name" or "lastname" or "useremail" or "age" or "usercity":
+        # print(field)
+        # print(new_detail)
+        change_some(username, field, new_detail)
     else:
-        print("this username is not available")
+        print("the field is uncorrected")
 
 
-dat = {
-    "username": "aviviti",
-    "name": "avivit",
-    "lastname": "noa",
-    "useremail": "avivitnol@gmail.com",
-    "age": "22",
-    "userpassword": "123456789",
-    "usercity": "ashdod"
-}
-print(dat)
-create_new_user(dat)
+def change_username(old_username, new_username):
+    if username_availble(new_username):
+        change_some(old_username, "username", new_username)
+    else:
+        print("the new user name is unavailable")
+
+
+def change_password(username, newpassword):
+    change_some(username, "userpassword", newpassword)
+
+
+# ------------------------ data for team ------------------------
+def new_team(team_name, username):
+    create_team(team_name, username)
+
+
+def get_team(teamid):
+    print(get_team_from_server(teamid))
+
+
+def new_user_for_team(username, teamid):
+    add_user_to_team(username, teamid)
+
+
+# ------------------------ data for tasks ------------------------
+
+def add_new_task(teamid, username, task, date=None):
+    set_task(teamid, username, task, date)
+
+
+def task_completed(teamid, taskid, username):
+    finish_task(teamid, taskid, username)
+
+
+# ------------------------ data for shoppinglist ------------------------
+
+
+# Setup flask server
+app = Flask(__name__)
+
+
+# def sql():
+#     db = MySQLdb.connect("localhost","root","","sql8591008")
+#     insertrec=db.cursor()
+#     sqlquery="insert into users()"
+
+
+# Setup url route which will calculate
+# total sum of array.
+@app.route('/project/register', methods=['POST'])
+def sum_of_array():
+    data = request.get_json()
+    res = create_new_user(data)
+    print(res)
+    print(data)
+
+    # Data variable contains the
+    # data from the node server
+    # ls = data['array']
+
+    # calculate the sum
+
+    # Return data in json format
+    return jsonify(success=True)
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
+    # check_email("amit255888888888888@gmail.com")
+    dat = {
+        "username": "avivush",
+        "name": "aviv",
+        "lastname": "lev",
+        "useremail": "avivlev@gmail.com",
+        "age": "34",
+        "userpassword": "123123123",
+        "usercity": "jerusalem",
+        "teams": []
+    }
+    # userdata = {
+    #     "username": "gadsh",
+    #     "name": "gad",
+    #     "lastname": "shrim",
+    #     "useremail": "gadsh@gmail.com",
+    #     "age": "30",
+    #     "userpassword": "11223344",
+    #     "usercity": "Ashdod",
+    #     "teams": []
+    # }
+    #
+    # teamid = "63b41d1edd3902090a55ef80"
+    # username = "aviviti"
+    # task = "buy a new computer"
+    # taskid = "63b41db0cf3bbd24a5a8b271"
+    # date = datetime.date(2023, 1, 12)
+    #
+    # create_new_user(dat)
+    # new_team("newteam", "aviviti")
+    # add_user_to_team("gadsh", teamid)
+    # add_new_task(teamid, username, task, date)
+    # task_completed(teamid, taskid, "gadsh")
+    # change_detail("aviviti", "lastname", "shoshi")
--- Server/ServerToDataBase.py
+++ Server/ServerToDataBase.py
@@ -1,3 +1,4 @@
+import datetime
 import pymongo
 from bson import ObjectId
 
@@ -5,24 +6,41 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["fproject"]
 
 
-# ------------ data for user ------------
+# ------------------------ data for user ------------------------
 def set_user_to_server(userdata):
     mycol = mydb["user"]
-    x = mycol.insert_one(userdata)
-    print(x)
+    mycol.insert_one(userdata)
 
 
-def get_user_to_server(username):
+def get_user_from_server(username):
     mycol = mydb["user"]
-    for x in mycol.find({}, {"_id": 1, "username": 1, "useremail": 1}):
+    for x in mycol.find({},
+                        {"_id": 1, "username": 1, "name": 1, "lastname": 1, "useremail": 1, "age": 1, "usercity": 1}):
+        if x.get("username") == username:
+            return x
+
+
+def get_userid_from_server(username):
+    mycol = mydb["user"]
+    for x in mycol.find({},
+                        {"_id": 1, "username": 1, "name": 1, "lastname": 1, "useremail": 1, "age": 1, "usercity": 1}):
         if x.get("username") == username:
             return x.get("_id")
 
 
+def username_availble(username):
+    mycol = mydb["user"]
+    for x in mycol.find({}, {"_id": 0, "username": 1}):
+        if x.get("username") == username:
+            return False
+    return True
+
+
 def change_some(username, some, somedetails):
     mycol = mydb["user"]
-    for x in mycol.find({}, {"_id": 0, "username": 1, "name": 1, "lastname": 1, "useremail": 1, "age": 1,
-                             "userpassword": 1, "usercity": 1}):
+    for x in mycol.find({},
+                        {"username": 1, "name": 1, "lastname": 1, "useremail": 1, "age": 1,
+                         "userpassword": 0, "usercity": 1}):
         if x.get("username") == username:
             myquery = {some: x.get(some)}
             newvalues = {"$set": {some: somedetails}}
@@ -32,80 +50,109 @@ def change_some(username, some, somedetails):
     #     print(x)
 
 
-# ------------ data for team ------------
-
-def get_team_to_server(team):
-    mycol = mydb["team"]
-    for x in mycol.find({}, {"_id": 1}):
-        if x.get("teamid") == team:
-            return x.get("_id")
-
+# ------------------------ data for team ------------------------
 
 def create_team(team_name, username):
     mycol = mydb["team"]
-
+    _id = get_userid_from_server(username)
     team = {
         "teamname": team_name,
         "users": [],
-        "manager": username
+        "manager": _id
     }
-    team["users"].append(username)
+    team["users"].append(_id)
     x = mycol.insert_one(team)
-    print(x.inserted_id)
     mycol = mydb["user"]
-    _id = get_user_to_server(username)
-    print(type(x.inserted_id))
-    print(type(_id))
     mycol.update_one({'_id': _id}, {'$push': {'teams': x.inserted_id}})
 
 
-def add_user_to_team(username, team):
+def get_team_from_server(teamid):
     mycol = mydb["team"]
-    oid2 = ObjectId(team)
-    print(oid2)
-    x = mycol.update_one({'_id': oid2}, {'$push': {'users': username}})
+    for x in mycol.find({}, {"_id": 1}):
+        if x.get("teamid") == teamid:
+            return x
 
 
-def set_task(team, username, task):
+def add_user_to_team(username, teamid):
     mycol = mydb["team"]
-    # print(mycol["tasks"])
-    oid2 = ObjectId(team)
+    oid2 = ObjectId(teamid)
+    _id = get_userid_from_server(username)
+    mycol.update_one({'_id': oid2}, {'$push': {'users': _id}})
+
+
+# ------------------------ data for tasks ------------------------
+
+
+def set_task(teamid, username, task, date=None):
+    mycol = mydb["team"]
+    oid2 = ObjectId(teamid)
+    sdate = str(date)
     mycol.update_one({'_id': oid2},
-                     {'$push': {'tasks': {"_ID": ObjectId(), "username": username, "task": task, "done": False}}})
+                     {'$push': {'tasks': {"_ID": ObjectId(), "username": username, "task": task, "done": False,
+                                          "date": sdate}}})
 
 
-def finish_task(teamid, taskid):
+def finish_task(teamid, taskid, username):
     oid1 = ObjectId(teamid)
     oid2 = ObjectId(taskid)
     mycol = mydb["team"]
+    cdate = datetime.date.today()
+    current_date = str(cdate)
+    _id = get_userid_from_server(username)
     mycol.update_one({"_id": oid1, "tasks._ID": oid2}, {"$set": {"tasks.$.done": True}})
+    mycol.update_one({"_id": oid1, "tasks._ID": oid2}, {"$set": {"tasks.$.fdate": current_date}})
+    mycol.update_one({"_id": oid1, "tasks._ID": oid2}, {"$set": {"tasks.$.ftask": _id}})
+
+
+# ------------------------ data for shoppinglist ------------------------
+def create_shopping_list(teamid, username, products, date=None):
+    mycol = mydb["team"]
+    oid2 = ObjectId(teamid)
+    sdate = str(date)
+    mycol.update_one({'_id': oid2},
+                     {'$push': {'shlists': {"_ID": ObjectId(), "username": username, "products": products, "done": False,
+                                           "date": sdate}}})
+
+
+def finish_shopping_list(teamid, listid, username):
+    oid1 = ObjectId(teamid)
+    oid2 = ObjectId(listid)
+    mycol = mydb["team"]
+    cdate = datetime.date.today()
+    current_date = str(cdate)
+    _id = get_userid_from_server(username)
+    mycol.update_one({"_id": oid1, "tasks._ID": oid2}, {"$set": {"shlists.$.done": True}})
+    mycol.update_one({"_id": oid1, "tasks._ID": oid2}, {"$set": {"shlists.$.fdate": current_date}})
+    mycol.update_one({"_id": oid1, "tasks._ID": oid2}, {"$set": {"shlists.$.fshlist": _id}})
+
+def all_products():
+    mycol = mydb["team"]
+    myprod = mydb["all_products"]
+    prod = myprod.find()
+    for x in mycol.find({},
+                        {"teamname": 1, "manager": 1, "shlist": 1}):
+        p = x['shlist'][0]['products']
+        for i in p:
+            for j in prod:
+                if j == i:
+                    break
 
 
 if __name__ == '__main__':
     # test: set new task
-    task = "go shopping "
+    # task = "shopping "
+    # date = datetime.date(2018, 5, 12)
+    # print(date)
+    # print(type(date))
     # set_task("63b2cdcc8fc4e9d0bd870068", "gadsh", task)
-
     # test:change something in user data
-    # change_some("avivi", "lastname", "levi")
+    # change_some("aviviti", "lastname", "lev")
 
-    userdata = {
-        "username": "gadsh",
-        "name": "gad",
-        "lastname": "shrim",
-        "useremail": "gadsh@gmail.com",
-        "age": "30",
-        "userpassword": "11223344",
-        "usercity": "Gilo",
-        "teams": []
+    products = {
+        "חלב": "1",
+        "לחם": "2",
+        "גבינה": "6"
     }
-    # username = "amitush"
-    # set_user_to_server(userdata)
-
-    # test: create new team
-    # create_team("besty", "gadsh")
-
-    # add user to your team
-    # add_user_to_team("amitush", "63b2cdcc8fc4e9d0bd870068")
-
-    finish_task("63b2cdcc8fc4e9d0bd870068", "63b2cf3da64ce445adc94d0e")
+    teamid = "63b41d1edd3902090a55ef80"
+    # create_shopping_list(teamid, "gadsh", products)
+    # all_products()
